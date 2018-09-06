14950e0e9b2398117090def59de8674ad26758f2
Daniela Beltran <ing.danibeltran@gmail.com>
2018-09-05 19:20:50 -0500

    Cambios

--- src/index.js
+++ src/index.js
@@ -23,9 +23,7 @@ class Square extends React.Component {
     }
 
     renderSquare(i) {
-      return <Square 
-            value={this.state.squares[i]}
-            onClick={() => this.handleClick(i)}/>;
+      return <Square value={i}/>;
     }
   
     render() {
